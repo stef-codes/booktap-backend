3996526a99276054884f4124c6774baed16a488a
Stefanie <stefcodes@gmail.com>
2020-02-11 10:21:58 -0800

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_11_171210) do
+ActiveRecord::Schema.define(version: 2020_02_11_181920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 2020_02_11_171210) do
     t.string "type"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "book_id"
   end
 
+  add_foreign_key "comments", "books"
 end
