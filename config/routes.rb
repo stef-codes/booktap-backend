02682176c1a2a95297434ae656a3ded81d49552e
Stefanie <stefcodes@gmail.com>
2020-02-13 15:00:40 -0800

    nest comments under books

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   resources :comments
   resources :books
+
+  #nested routes
+  resources :books, only: [:index, :show] do
+    resources :comments
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
