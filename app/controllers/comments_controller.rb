85f3c55489d3f5c5879f2464615cd3ec14752465
Stefanie <stefcodes@gmail.com>
2020-02-13 15:00:13 -0800

    convert instance variables to local variables

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,45 +3,45 @@ class CommentsController < ApplicationController
 
   # GET /comments
   def index
-    @comments = Comment.all
+     comments = Comment.all
 
-    render json: @comments
+    render json:  comments
   end
 
   # GET /comments/1
   def show
-    render json: @comment
+    render json:  comment
   end
 
   # POST /comments
   def create
-    @comment = Comment.new(comment_params)
+     comment = Comment.new(comment_params)
 
-    if @comment.save
-      render json: @comment, status: :created, location: @comment
+    if  comment.save
+      render json:  comment, status: :created, location:  comment
     else
-      render json: @comment.errors, status: :unprocessable_entity
+      render json:  comment.errors, status: :unprocessable_entity
     end
   end
 
   # PATCH/PUT /comments/1
   def update
-    if @comment.update(comment_params)
-      render json: @comment
+    if  comment.update(comment_params)
+      render json:  comment
     else
-      render json: @comment.errors, status: :unprocessable_entity
+      render json:  comment.errors, status: :unprocessable_entity
     end
   end
 
   # DELETE /comments/1
   def destroy
-    @comment.destroy
+     comment.destroy
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_comment
-      @comment = Comment.find(params[:id])
+       comment = Comment.find(params[:id])
     end
 
     # Only allow a trusted parameter "white list" through.
