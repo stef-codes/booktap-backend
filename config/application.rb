82109e96222e218eebf797c676d4b611f247af06
Stefanie <stefcodes@gmail.com>
2020-02-14 13:08:03 -0800

    add cors to application

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,16 @@ module BooktapBackend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    Rails.application.config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+    
+        resource '*',
+          headers: :any,
+          methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
