7af313204f750cb43ef9bd6c75ad2f91c92e8392
Stefanie <stefcodes@gmail.com>
2020-02-18 18:52:59 -0800

    add validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
     belongs_to :book
+    validates :content, presence: true
+    validates :content, uniqueness: true
+    validates :comment_type, presence: true
 end
